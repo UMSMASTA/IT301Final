40779341643903baa44e7ea83501b3d9d30b0ecc
UMSMASTA <91797310+UMSMASTA@users.noreply.github.com>
2024-11-17 10:07:21 -0800

    Update Untitled1.ps1

--- Untitled1.ps1
+++ Untitled1.ps1
@@ -10,6 +10,7 @@ New-Item -ItemType Directory -Path "C:\VHDs"
 New-VHD -Path "C:\VHDs\MyVM.vhdx" -SizeBytes 40GB -Dynamic
 New-VM -Name "IT301Final" -MemoryStartupBytes 2GB -VHDPath "C:\VHDs\MyVM.vhdx"
 Set-VM -Name "IT301Final" -ProcessorCount 2
+#Make sure that the Path matches where you downloaded the ISO
 Set-VMDvdDrive -VMName "IT301Final" -Path "C:\Users\Administrator\Desktop\Windows_Server_2016_Unattended_Bootable.iso"
 
 # Ensure the VM is running, may need to do a wait here to allow the VM to boot up before making the users in the VM and other tasks?
